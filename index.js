fdc06b76e51ceba62ed5cec671b544da51079c6b
LiBai666 <overwall2016@gmail.com>
2018-06-16 10:44:55 +0800

    使用正则表达式匹配路由

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const http = require("http");
 class rabbit {
     constructor(){
         this.afterReqArrayFuns = [];
-        this.router = {
-            "GET":{},
-            "POST":{},
-        }
+        this.beforeResArrayFuns = [];
+        this.routerArr = [];
     }
 
     listen(port){
@@ -16,15 +14,28 @@ class rabbit {
     getCallbackFun(){
         let that =this;
         return function (req,res) {
+
             that.afterReqArrayFuns.forEach(fun => {         // 不同的req中间件可能冲突，不方便维护扩展啊，难道只能不断测试？
                 fun(req)
             });
-            res.write(JSON.stringify(req.query));
-            //res.write(JSON.stringify(req.cookie))
 
-            let path = req.url.split("?")[0];
-            let callback = that.router[req.method] ? (that.router[req.method][path] || that.NotExistUrl) : that.NotExistUrl;
-            callback(req,res);
+            /*
+            匹配路由
+            提取url中数据
+            执行响应函数
+            */
+            for(let router of that.routerArr){
+                if(router.method.toLocaleLowerCase() == req.method.toLocaleLowerCase() 
+                                    && router.pathReg.test(req.url.split("?")[0])){
+                    router.callback(req,res)
+                    break
+                }
+            }
+
+            that.beforeResArrayFuns.forEach(fun => {         // 不同的req中间件可能冲突，不方便维护扩展啊，难道只能不断测试？
+                fun(req)
+            });
+            that.NotExistUrl(req,res)
         }
     }
 
@@ -33,16 +44,27 @@ class rabbit {
             this.afterReqArrayFuns.push(arguments[i])
         }
     }
-
-    get(path,callback) {
-        this.router["GET"][path] = callback
+    beforeRes(){
+        for(let i = 0;i<arguments.length;i++){
+            this.beforeResArrayFuns.push(arguments[i])
+        }
     }
-    post(path,callback){
-        this.router["POST"][path] = callback
+
+    router(method,pathReg,callback) {
+        let obj = {
+            method,
+            pathReg,
+            callback
+        }
+        this.routerArr.push(obj)
     }
 
     NotExistUrl(req,res){
-        res.end('Not found')
+        try{
+            res.end("Not Exist Url")
+        }catch(e){
+            break
+        }
     }
 }
 
